bccfa351423c5b26487b9d303b0e94cd77b6c9c0
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-07-22 11:33:31 +0000

    Styling Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,14 @@ import avatar from '../temp/avatar.png'
 
 // Style
 const style = {
-    wrapper: `h-16 w-full bg-black text-white flex md: justify-around items-center px-60 fixed z-20 font-family:`
+    wrapper: `h-16 w-full bg-black text-white flex md: justify-around items-center px-60 fixed z-20`,
+    leftMenu: `flex gap-3`,
+    logo: `text-3xl text-white flex cursor-pointer mr-16`,
+    menuItem: `text-lg text-white font-medium flex items-center mx-4 cursor-pointer`,
+    rightMenu: `flex gap-3 items-center`,
+    userImageContainer: `mr-2`,
+    userImage: `h-10 w-10 mr-4 rounded-full p-px object-cover cursor-pointer`
+
 }
 const Navbar = () => {
     return (
@@ -18,9 +25,14 @@ const Navbar = () => {
                 <div className={style.menuItem}>Help</div>
                 <div className={style.menuItem} id = "userPerson">Lex</div>
                 <div className={style.userImageContainer}>
-                    <Image src={avatar} width ={40} height = {40}/>
+                    <Image 
+                    className={style.userImage}
+                    src={avatar} 
+                    width ={40} 
+                    height = {40}/>
                 </div>
             </div>
+            <div>0x0000...0000</div>
         </div>
     )
 }
