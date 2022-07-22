81c4d2a82606b71d76fce69a12a34418b02d154f
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-07-22 12:54:10 +0000

    Adding Mapbox

--- components/Map.js
+++ components/Map.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import mapboxgl from "mapbox-gl"
+import { useEffect } from "react"
+ 
+const style = {
+    wrapper: `flex-1 h-full w-full`
+}
+
+mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+
 
 const Map = () => {
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/drakosi/ckvcwg3rwdw4314o3i2ho8tph',
+            center: [-99.29011, 39.39172],
+            zoom: 2.5,
+        })
+    })
+
   return (
-    <div>Map</div>
+    <div id="map">Map</div>
   )
 }
 
