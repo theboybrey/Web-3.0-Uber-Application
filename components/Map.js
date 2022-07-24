196ddc1029a17362299763ff043aeef206cab287
Lex Benjamin <abessilfie@st.ug.edu.gh>
2022-07-24 12:58:14 +0000

    Map.js update

--- components/Map.js
+++ components/Map.js
@@ -11,12 +11,13 @@ mapboxgl.accessToken = 'pk.eyJ1IjoidGhlYm95YnJleSIsImEiOiJjbDV3Z2NnMDgwNHN0M2ZvY
 const Map = () => {
     useEffect(() => {
         const map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/drakosi/ckvcwg3rwdw4314o3i2ho8tph',
-            center: [-99.29011, 39.39172],
-            zoom: 3,
-            projection: 'globe',
-        })
+            container: 'map', // container ID
+            style: 'mapbox://styles/theboybreyy/ckvcwq3rwdw4314o3i2ho8tph', // style URL
+            center: [-99.29011, 39.39172], // starting position [lng, lat]
+            zoom: 3, // starting zoom
+            projection: 'globe' // display the map as a 3D globe
+            });
+            
     })
 
   return (
